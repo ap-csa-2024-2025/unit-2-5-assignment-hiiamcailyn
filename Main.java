2124a04bb9aab497d0ff94a82059940b3afef060
hiiamcailyn <cailyntran@gmail.com>
2024-10-07 09:57:37 -0700

    hi

--- Main.java
+++ Main.java
@@ -5,12 +5,12 @@ public class Main
   public static void main(String[] args)
   {
     Circle circ1 = new Circle(10.1);
-    Circle circ2 = new Circleircle(14.0);
+    Circle circ2 = new Circle(14.0);
     Circle circ3 = new Circle(20.5);
 
-    System.out.println(circ1.toString());  
-    System.out.println(circ2.toString());  
-    System.out.println(circ3.toString());  
+    System.out.println(circ1);  
+    System.out.println(circ2);  
+    System.out.println(circ3);  
 
 
   }
